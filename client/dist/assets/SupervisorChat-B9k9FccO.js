adf734095fb12813624fb200212d58e26e6cf535
Rauf0505 <chabdulrauf92@gmail.com>
2026-07-08 02:12:17 +0500

    chore: remove node_modules, dist, .env.production from tracking
    - Removed client/node_modules/ from git
    - Removed server/node_modules/ from git
    - Removed client/dist/ build artifacts from git
    - Removed client/.env.production from git
    - Updated .gitignore to prevent re-tracking

--- client/dist/assets/SupervisorChat-B9k9FccO.js
+++
@@ -1,1 +0,0 @@
-import{C as t}from"./ChatWindow-bQsyl1iC.js";import"./index-r-SdLOd9.js";export{t as default};
--- client/dist/assets/SupervisorProfile-Bw_pxFRG.js
+++
@@ -1,1 +0,0 @@
-import o from"./StudentProfile-BsXjv6Fy.js";import"./index-r-SdLOd9.js";import"./PageHeader-NoeocOTL.js";export{o as default};
